d3397432267279ea3688ed28c4947e365adeaefb
Pranoy Biswas Bappa <75092679+impranoybiswas@users.noreply.github.com>
2026-03-08 16:08:09 +0600

    pettern genarater

---
+++ TrianglePattern.ts
@@ -0,0 +1,10 @@
+const trianglePattern = (n: number, s: string = "#") => {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            result += s + " ";
+        }
+        result += "\n";
+    }
+    return result;
+}
