7e7e9c4ac024b6f1ea67fd4011b7dd925a76fece
Pranoy Biswas Bappa <75092679+impranoybiswas@users.noreply.github.com>
2026-03-10 13:00:49 +0600

    add map method

---
+++ MapMethod.ts
@@ -0,0 +1,10 @@
+// Map Method
+
+const students = [
+  { id: 4, name: "John", age: 24 },
+  { id: 5, name: "Jane", age: 23 },
+  { id: 6, name: "Mark", age: 25 },
+];
+
+
+students.map((student) => console.log({Name : student.name, Age : student.age}));
