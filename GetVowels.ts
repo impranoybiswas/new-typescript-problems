5722394d8369e3f67c285d32841b0bf7efe3c45b
Pranoy Biswas Bappa <75092679+impranoybiswas@users.noreply.github.com>
2026-03-11 16:07:51 +0600

    upadte get vowels

--- GetVowels.ts
+++ GetVowels.ts
@@ -1,9 +1,26 @@
-function getVowels (str: string) {
-    return str.match(/[aeiou]/gi) || []; 
+function getVowels(str: string) {
+  return str.match(/[aeiou]/gi) || [];
 }
 
-function getVowelsLength (str: string) {
-    return getVowels(str).length || 0; 
+function getVowelsManual(str: string) {
+  let vowelCount = 0;
+  let stringArr = str.toLowerCase().split("");
+  for (let i = 0; i < stringArr.length; i++) {
+    if (
+      stringArr[i] === "a" ||
+      stringArr[i] === "e" ||
+      stringArr[i] === "i" ||
+      stringArr[i] === "o" ||
+      stringArr[i] === "u"
+    ) {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
 }
 
-console.log(getVowelsLength("javascript"));
+function getVowelsLength(str: string) {
+  return getVowels(str).length || 0;
+}
+
+console.log(getVowelsManual("javascript"));
